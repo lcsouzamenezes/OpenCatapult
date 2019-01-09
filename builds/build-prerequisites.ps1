d5cfd13faa265d3512eb6df3a3f3ebbfeca1b957
BrainBalaka <xainsz.balaka@gmail.com>
2019-01-09 19:00:46 +0700

    Automatically install dotnet in build-prerequisites script (#265)

--- builds/build-prerequisites.ps1
+++ builds/build-prerequisites.ps1
@@ -5,11 +5,28 @@ $allGood = $true
 
 $currentSdkVersion = dotnet --version
 
-if ([System.Version]$currentSdkVersion -lt $dotnetSdkVersion) {
-    Write-Output "You need to install dotnet SDK version $dotnetSdkVersion."
-    Write-Host "Please run this command in an elevated shell: .\builds\dotnet-install.ps1 -Channel 2.1 -InstallDir `$env:ProgramFiles\dotnet"
-
-    $allGood = $false
+if ([System.Version]$currentSdkVersion -lt $dotnetSdkVersion) {	
+	$allGood = $false
+	Write-host "We need to install the dotnet SDK version $dotnetSdkVersion for you. Proceed? (y/n)" -ForegroundColor Yellow 
+	$readInstallDotnet = Read-Host
+	Switch ($readInstallDotnet) { 
+		Y {$installDotnet=$true} 
+		N {$installDotnet=$false} 
+		Default {Write-Host "Invalid input" -ForegroundColor Red; $installDotnet=$false} 
+	} 
+	
+	if ($installDotnet) {
+		if (Test-Path ".\builds\dotnet-install.ps1") { 
+			.\builds\dotnet-install.ps1 -Channel 2.1 -InstallDir $env:ProgramFiles\dotnet
+		}
+		elseif (Test-Path ".\dotnet-install.ps1") { 
+			.\dotnet-install.ps1 -Channel 2.1 -InstallDir $env:ProgramFiles\dotnet
+		}
+		else { 
+			Write-Host "Cannot find dotnet-install.ps1" -ForegroundColor Red
+			Exit 1
+		}
+	}
 }
 
 if ($allGood){
