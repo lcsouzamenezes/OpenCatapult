4d6558f7251ae2fec1e672aa532ebdc76a0e394e
BrainBalaka <xainsz.balaka@gmail.com>
2019-01-11 18:15:18 +0700

    remove old built-in plugins and abstractions (#282)

--- src/Plugins/Polyrific.Catapult.Plugins.Abstraction/Configs/PublishArtifactTaskConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Polyrific, Inc 2018. All rights reserved.
-
-namespace Polyrific.Catapult.Plugins.Abstraction.Configs
-{
-    public class PublishArtifactTaskConfig : BaseJobTaskConfig
-    {
-        
-    }
-}
