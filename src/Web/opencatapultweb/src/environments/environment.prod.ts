94941e000fdb3b6e863b7f4a942637c95f184048
Frandi Dwi <dev@rumahfrandi.com>
2019-02-21 12:45:42 +0700

    Add version field to environment.ts (#350)

--- src/Web/opencatapultweb/src/environments/environment.prod.ts
+++ src/Web/opencatapultweb/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:44305'
+  apiUrl: 'https://localhost:44305',
+  version: '1.0.0-beta2'
 };
--- src/Web/opencatapultweb/src/environments/environment.ts
+++ src/Web/opencatapultweb/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44305'
+  apiUrl: 'https://localhost:44305',
+  version: '1.0.0-beta2'
 };
 
 /*
