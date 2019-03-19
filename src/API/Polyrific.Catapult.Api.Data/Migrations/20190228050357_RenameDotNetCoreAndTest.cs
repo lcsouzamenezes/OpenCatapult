3934357f1d301b4cfffb4a392b6063697f46569d
BrainBalaka <xainsz.balaka@gmail.com>
2019-03-19 18:10:33 +0700

    Fix migration script where it does not properly rename the DotNetCore plugin (#411)

--- src/API/Polyrific.Catapult.Api.Data/Migrations/20190228050357_RenameDotNetCoreAndTest.cs
+++ src/API/Polyrific.Catapult.Api.Data/Migrations/20190228050357_RenameDotNetCoreAndTest.cs
@@ -9,6 +9,13 @@ namespace Polyrific.Catapult.Api.Data.Migrations
             migrationBuilder.UpdateData(
                 table: "Plugins",
                 keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "Name" },
+                values: new object[] { "976e0533-360a-4e46-8220-7c1cfdf0e0a3", "Polyrific.Catapult.TaskProviders.DotNetCore" });
+
+            migrationBuilder.UpdateData(
+                table: "Plugins",
+                keyColumn: "Id",
                 keyValue: 4,
                 columns: new[] { "ConcurrencyStamp", "Name" },
                 values: new object[] { "976e0533-360a-4e46-8220-7c1cfdf0e0a4", "Polyrific.Catapult.TaskProviders.DotNetCoreTest" });
@@ -16,6 +23,13 @@ namespace Polyrific.Catapult.Api.Data.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.UpdateData(
+                table: "Plugins",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "Name" },
+                values: new object[] { "976e0533-360a-4e46-8220-7c1cfdf0e0a3", "Polyrific.Catapult.Plugins.DotNetCore" });
+
             migrationBuilder.UpdateData(
                 table: "Plugins",
                 keyColumn: "Id",
