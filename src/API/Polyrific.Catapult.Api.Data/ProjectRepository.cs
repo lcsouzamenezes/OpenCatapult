3ebb89cec9dc04564ecba2076507475b85a54e36
BrainBalaka <xainsz.balaka@gmail.com>
2019-05-15 11:50:01 +0700

    Fix issue in delete project when using sqlite (#545)
    * Fix issue in delete project when using sqlite
    
    * Manually delete project properties

--- src/API/Polyrific.Catapult.Api.Data/ProjectRepository.cs
+++ src/API/Polyrific.Catapult.Api.Data/ProjectRepository.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (c) Polyrific, Inc 2018. All rights reserved.
 
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Polyrific.Catapult.Api.Core.Entities;
 using Polyrific.Catapult.Api.Core.Repositories;
 
@@ -13,6 +16,20 @@ namespace Polyrific.Catapult.Api.Data
 
         public ProjectRepository(CatapultSqliteDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public override async Task Delete(int id, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            // Remove all data model properties to avoid FK conflict of "RelatedProjectDataModel"
+            var properties = Db.Set<ProjectDataModelProperty>().Where(p => p.ProjectDataModel.ProjectId == id);
+            Db.ProjectDataModelProperties.RemoveRange(properties);
+
+            var dbSet = Db.Set<Project>();
+            var entity = await dbSet.FindAsync(id);
+            dbSet.Remove(entity);
+            await Db.SaveChangesAsync(cancellationToken);
         }
     }
 }
