fd6a2923131017bb724da638be5e9f6de126db33
Frandi Dwi <frandi.dwijayanto@polyrific.com>
2019-05-17 10:55:01 +0700

    Small adjustment on the intro message (#554)

--- tools/scripts/run-engine.bat
+++ tools/scripts/run-engine.bat
@@ -4,6 +4,6 @@ echo  =======================
 echo    OpenCatapult Engine
 echo  =======================
 echo.
-echo  Please run "ocengine.exe --help" for available commands, or check https://docs.opencatapult.net for more details.
+echo  Please run "ocengine --help" for available commands, or check https://docs.opencatapult.net for more details.
 
 cd engine
